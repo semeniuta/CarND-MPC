01a27ab2731612c151c393333e175407a089e70b
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-12-26 20:10:13 +0100

    Implementation of WaypointsMap, readWaypoints, invertPose, createPose, distance

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "geometry.h"
+#include <string>
+#include <fstream>
+#include <vector>
 
 // Evaluate a polynomial.
 double polyeval(const Eigen::VectorXd& coeffs, double x) {
@@ -12,7 +15,7 @@ double polyeval(const Eigen::VectorXd& coeffs, double x) {
   for (int i = 0; i < coeffs.size(); i++) {
     result += coeffs[i] * pow(x, i);
   }
-  
+
   return result;
 }
 
@@ -38,4 +41,91 @@ Eigen::VectorXd polyfit(const Eigen::VectorXd& xvals, const Eigen::VectorXd& yva
   auto Q = A.householderQr();
   auto result = Q.solve(yvals);
   return result;
+
+}
+
+WaypointsMap readWaypoints(const std::string& filename) {
+
+  std::ifstream in_file(filename.c_str(), std::ifstream::in);
+
+  if (!in_file.is_open()) {
+    throw std::runtime_error("Cannot open input file");
+  }
+
+  std::vector<double> x_vec;
+  std::vector<double> y_vec;
+
+  std::string line;
+  double x;
+  double y;
+
+  while(getline(in_file, line)){
+
+    std::istringstream iss{line};
+    iss >> x;
+    iss >> y;
+
+    x_vec.push_back(x);
+    y_vec.push_back(y);
+
+  }
+
+  WaypointsMap map;
+  map.x = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(x_vec.data(), x_vec.size());
+  map.y = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(y_vec.data(), y_vec.size());
+
+  return map;
+
+}
+
+Eigen::MatrixXd createPose(double x, double y, double theta) {
+
+  double ct = cos(theta);
+  double st = sin(theta);
+
+  Eigen::MatrixXd pose{3, 3};
+
+  pose << ct, -st, x,
+          st,  ct, y,
+           0,   0, 1;
+
+  return pose;
+
+}
+
+Eigen::MatrixXd invertPose(const Eigen::MatrixXd& pose) {
+
+  //Eigen::MatrixXd pose_inv = pose.inverse();
+
+  Eigen::MatrixXd pose_inv{3, 3};
+
+  double ct = pose(0, 0);
+  double st = pose(1, 0);
+
+  double x = pose(0, 2);
+  double y = pose(1, 2);
+
+  pose_inv << ct, st, -x*ct - y*st,
+             -st, ct,  x*st - y*ct,
+               0,  0,            1;
+
+  return pose_inv;
+
+}
+
+double distance(const Eigen::VectorXd& x1, const Eigen::VectorXd& x2) {
+
+  auto n = x1.size();
+
+  double ss{0.};
+
+  for (unsigned int i = 0; i < n; i++) {
+
+    double diff = x1(i) - x2(i);
+
+    ss += diff * diff;
+  }
+
+  return sqrt(ss);
+
 }
--- src/geometry.h
+++ src/geometry.h
@@ -7,14 +7,44 @@
 
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
+#include "Eigen-3.3/Eigen/Dense"
+
+
+struct WaypointsMap {
+
+  Eigen::VectorXd x;
+  Eigen::VectorXd y;
+
+  Eigen::VectorXd transformPoint(unsigned int i, const Eigen::MatrixXd& pose) {
+
+    Eigen::VectorXd pt{x[i], y[i]};
+
+    Eigen::VectorXd pt_transformed = pose * pt;
+
+    return Eigen::VectorXd{
+        pt_transformed(0) / pt_transformed(2),
+        pt_transformed(1) / pt_transformed(2),
+    };
+
+  }
+
+};
 
 // For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
+//constexpr double pi() { return M_PI; }
+//double deg2rad(double x) { return x * pi() / 180; }
+//double rad2deg(double x) { return x * 180 / pi(); }
 
 double polyeval(const Eigen::VectorXd& coeffs, double x);
 
 Eigen::VectorXd polyfit(const Eigen::VectorXd& xvals, const Eigen::VectorXd& yvals, int order);
+
+WaypointsMap readWaypoints(const std::string& filename);
+
+Eigen::MatrixXd invertPose(const Eigen::MatrixXd& pose);
+
+Eigen::MatrixXd createPose(double x, double y, double theta);
+
+double distance(const Eigen::VectorXd& x1, const Eigen::VectorXd& x2);
 
 #endif //GEOMETRY_H
